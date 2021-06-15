1885429dd5d0a7eba1a64cc4b27892522a16501a
Shivu02062002 <75877929+Shivu02062002@users.noreply.github.com>
2021-06-15 08:53:30 +0530

    Almost completed the skills page

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -8,6 +8,7 @@ import jsSkill from "./jsSkill.png";
 import nodeSkill from "./nodeSkill.png";
 import pythonSkill from "./pythonSkill.png";
 import reactSkill from "./reactSkill.png";
+import mongoSkill from "./mongoSkill.png";
 
 function Skills() {
     return (
@@ -15,20 +16,66 @@ function Skills() {
             <h1 className="skills-heading">Skills</h1>
             <Carousel className="carousel-area">
                 <Carousel.Item>
-                    <img src={cSkill} className="skills-img" alt="C Language" width="250" height="250" />
-                    <img src={cppSkill} className="skills-img" alt="C++ Language" width="250" height="250" />
-                    <img src={pythonSkill} className="skills-img" alt="Python Language" width="250" height="250" />
+                    <table>
+                        <tr>
+                            <td>
+                                <img src={cSkill} className="skills-img" alt="C Language" width="170" height="170" />
+                            </td>
+                            <td>
+                                <img src={cppSkill} className="skills-img" alt="C++ Language" width="170" height="170" />
+                            </td>
+                            <td>
+                                <img src={pythonSkill} className="skills-img" alt="Python Language" width="170" height="170" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>C Language</td>
+                            <td>C++ Language</td>
+                            <td>Python</td>
+                        </tr>
+                    </table>
                 </Carousel.Item>
 
                 <Carousel.Item>
-                    <img src={htmlSkill} className="skills-img" alt="HTML Language" width="250" height="250" />
-                    <img src={cssSkill} className="skills-img" alt="CSS Language" width="250" height="250" />
-                    <img src={jsSkill} className="skills-img" alt="JavaScript Language" width="250" height="250" />
+                    <table>
+                        <tr>
+                            <td>
+                                <img src={htmlSkill} className="skills-img" alt="HTML Language" width="170" height="170" />
+                            </td>
+                            <td>
+                                <img src={cssSkill} className="skills-img" alt="CSS Language" width="170" height="170" />
+                            </td>
+                            <td>
+                                <img src={jsSkill} className="skills-img" alt="JavaScript Language" width="170" height="170" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>HTML5</td>
+                            <td>CSS</td>
+                            <td>JavaScript</td>
+                        </tr>
+                    </table>
                 </Carousel.Item>
 
                 <Carousel.Item>
-                    <img src={nodeSkill} className="skills-img" alt="NodeJS Language" width="250" height="250" />
-                    <img src={reactSkill} className="skills-img" alt="React Language" width="250" height="250" />
+                    <table>
+                        <tr>
+                            <td>
+                                <img src={nodeSkill} className="skills-img" alt="NodeJS Language" width="170" height="170" />
+                            </td>
+                            <td>
+                                <img src={mongoSkill} className="skills-img" alt="MongoDB" width="170" height="170" />
+                            </td>
+                            <td>
+                                <img src={reactSkill} className="skills-img" alt="React Language" width="170" height="170" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>NodeJS</td>
+                            <td>MongoDB</td>
+                            <td>ReactJS</td>
+                        </tr>
+                    </table>
                 </Carousel.Item>
             </Carousel>
         </div>
