3c304e85ad4a810a986113943c47f2c2f6f94c57
Shivu02062002 <75877929+Shivu02062002@users.noreply.github.com>
2021-06-15 09:13:48 +0530

    Made some changes to the styles component of the body

--- src/components/introduction/Intro2.jsx
+++ src/components/introduction/Intro2.jsx
@@ -3,11 +3,12 @@ import React from "react";
 function Intro2() {
     return (
         <div className="intro-para">
-            <h2>Short Intro</h2>
-            <p>I am a ReactJS and NodeJS developer.</p>
-            <p>Currently learning Data Structures & Algorithms.</p>
-            <p>And..........</p>
-            <p>Love to be surrounded by codes on the screen </p>
+            <h2 className="intro-title">Short Intro</h2>
+            <p>⚜ Computer Science Student at Nirma University</p>
+            <p>⚜ I am a ReactJS and NodeJS developer</p>
+            <p>⚜ Currently learning Data Structures & Algorithms</p>
+            <p>⚜ And..........</p>
+            <p>⚜ Love to be surrounded by codes on the screen </p>
         </div>
     )
 }
